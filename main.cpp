07c1450ae8facb2eecf7c47aa97faf065d537737
KGeer <grin.esg@gmail.com>
2018-06-29 16:40:55 +1000

    all wrong

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 #include <vector>
 #include <algorithm>
 #include <map>
-
+#include <string>
 using namespace std;
 
-void printCompare(const map <string, vector<string>>& routes, const string& stop, const string& bus){
+void STOPS_FOR_BUS(const map <int, vector<string>>& routesInputOrder,
+                  const string& stop, const string& bus, const vector<string>& order){
     bool isExist = false;
-    for(const auto& route : routes){
-            vector<string> list = route.second;
-            for(const auto& stop_name : list){
-                if (stop_name == stop && bus != route.first){
-                    cout << route.first << " ";
+    for(const auto& route : routesInputOrder){
+            for(const auto& stop_name : route.second){
+                if (stop_name == stop && bus != order[route.first]){
+                    cout << order[route.first] << " ";
                     isExist = true;
                 }
             }
@@ -19,58 +19,61 @@ void printCompare(const map <string, vector<string>>& routes, const string& stop
     if (!isExist) cout << "no interchange";
 }
 
-void isBuses(const map <string, vector<string>>& routes, const string& stop, const vector<string>& order){
-    bool bus = false;
-    for(string number : order){
-
-        for (const auto& route : routes){
-            vector<string> list = route.second;
-            if(find(list.begin(), list.end(), stop) != list.end()){
-               cout << number << " ";
-               bus = true;
-               break;
-            }
+void BUSES_FOR_STOP(const map <int, vector<string>>& routesInputOrder,
+             const string& stop, const vector<string>& order){
+    for (const auto& route : routesInputOrder){
+        if(find(route.second.begin(), route.second.end(), stop) != route.second.end()){
+            cout << order[route.first] << " ";
+        }else {
+            cout << "No stop";
+            break;
         }
     }
-    if(!bus) cout << "No stop";
 }
 
+
 int main() {
-    int n;
+    int n, j(0);
+    string command, stop, bus;
     cin >> n;
+
     map <string, vector<string>> routes;
+    map <int, vector<string>> routesInputOrder;
     vector<string> order;
+
     for (int i = 0; i < n; i++){
-        string command;
         cin >> command;
 
         if(command == "NEW_BUS"){
-            string tmp, tmp_s;
-            vector <string> temp;
+            string busNumber, busStop;
+            cin >> busNumber;
+            order.push_back(busNumber);
+
             int c(0);
-            cin >> tmp;
             cin >> c;
+
             for (int i = 0; i < c; i++){
-                cin >> tmp_s;
-                temp.push_back(tmp_s);
+                cin >> busStop;
+                routes[busNumber].push_back(busStop);
+                routesInputOrder[j].push_back(busStop);
             }
-            order.push_back(tmp);
-            routes[tmp] = temp;
+            j++;
 
         }else if(command == "BUSES_FOR_STOP"){
-            string stop;
             cin >> stop;
-                isBuses(routes, stop, order);
+            if(!(routes.empty())){
+                BUSES_FOR_STOP(routesInputOrder, stop, order);
+            }else cout << "No stop";
             cout << endl;
+
         }else if (command == "STOPS_FOR_BUS"){
-            string bus;
             cin >> bus;
             if (routes.count(bus)==0){
                 cout <<"No bus\n";
             }else {
                 for(const auto& stop : routes[bus]){
                     cout << "Stop " << stop << ": ";
-                       printCompare(routes, stop, bus);
+                       STOPS_FOR_BUS(routesInputOrder, stop, bus, order);
                     cout << endl;
                 }
             }
